4e33751da841b62b7f8855057b5904bcb890a648
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-17 19:03:11 -0300

    att

--- src/style/index.js
+++ src/style/index.js
@@ -55,6 +55,10 @@ export const GlobalStyle = createGlobalStyle`
     cursor: pointer;
     
     &:hover{
-        opacity: 0.8s
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.6;
     }
     `;
