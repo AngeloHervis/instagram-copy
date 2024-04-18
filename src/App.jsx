6bbbf67eb6d6ecf62d764666b996bfb400d7b0ba
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-18 20:50:40 -0300

    Configurando o tema dark-night

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useState } from 'react';
+
 import { ThemeProvider } from "styled-components";
 import { NavBar } from "./components/navbar";
 import { Screen } from "./style/index";
@@ -6,10 +7,17 @@ import { darkTheme, lightTheme } from "./style/theme";
 
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+
+  };
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <Screen>
-        <NavBar />
+        <NavBar themeToggler={themeToggler} theme ={theme} />
       </Screen>
     </ThemeProvider>
   );
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import {BsSun} from "react-icons/bs";
+import {BiMoon} from "react-icons/bi";
 import * as C from "./style";
 import { Flex } from "../../style/index";
 
-export function NavBar() {
+export function NavBar({themeToggler, theme}) {
     return <C.Container>
     <Flex> 
-        <C.BtnTheme>
-            <BsSun />
+        <C.BtnTheme onClick = {themeToggler}>
+            {theme === 'light' ? <BiMoon /> : <BsSun />}
         </C.BtnTheme> 
     </Flex>
     </C.Container>
