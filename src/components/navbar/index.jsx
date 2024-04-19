9bcdb5cdc3ec5bf3a2e538be2f1ffd6f9da4fb31
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-19 17:22:00 -0300

    Criação de função para economia de linhas

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,23 @@
 import {BsSun} from "react-icons/bs";
 import {BiMoon} from "react-icons/bi";
-
+import {Typography} from "../../style";
+import {Spacer} from "../../style";
 import LogoInstagram from "../../assets/logo-instagram.svg";
-
 import * as C from "./style";
-import { Flex } from "../../style/index";
+import { Flex } from "../../style";
 
 export function NavBar({themeToggler, theme}) {
-    return <C.Container>
+    function GroupText({title, subTitle}) {
+        return (
+            <Flex gap="4px">
+                <Typography>{title}</Typography>
+                <Typography weight="300" size="12px" height="14px">{subTitle}</Typography>
+            </Flex>
+        );
+    }
+
+    return (
+    <C.Container>
     <Flex> 
         <C.BtnTheme onClick = {themeToggler}>
             {theme === 'light' ? <BiMoon /> : <BsSun />}
@@ -20,10 +30,17 @@ export function NavBar({themeToggler, theme}) {
         <C.Profile>
             <img src="https://avatars.githubusercontent.com/u/109084569?v=4" alt="Imagem Perfil Github" />
         </C.Profile>
-        <Flex gap="4px">
-            <Typography>Angelo Hervis</Typography>
-            <Typography weight="300" size="12px" height="14px" >@Angelo_hervis</Typography>
-        </Flex>
+
+        <GroupText title = "Angelo Hervis" subTitle = "@Angelo_hervis"/>
+    </Flex>
+
+    <Spacer margin="8px"/>
+
+    <Flex direction="row" justify="space-between">
+        <GroupText title = "100" subTitle = "Publicações"/>
+        <GroupText title = "1627" subTitle = "Seguidores"/>
+        <GroupText title = "500" subTitle = "Seguindo"/>
     </Flex>
     </C.Container>
+    );
 }
