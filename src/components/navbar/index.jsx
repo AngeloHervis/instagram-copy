8c5bc5c95a645cd7d7e2d782ee7ca47b96bd2604
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-19 17:00:02 -0300

    att

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,8 @@
 import {BsSun} from "react-icons/bs";
 import {BiMoon} from "react-icons/bi";
+
+import LogoInstagram from "../../assets/logo-instagram.svg";
+
 import * as C from "./style";
 import { Flex } from "../../style/index";
 
@@ -9,6 +12,18 @@ export function NavBar({themeToggler, theme}) {
         <C.BtnTheme onClick = {themeToggler}>
             {theme === 'light' ? <BiMoon /> : <BsSun />}
         </C.BtnTheme> 
+    </Flex>
+
+    <img src={LogoInstagram} alt="Logo Instagram" />
+
+    <Flex>
+        <C.Profile>
+            <img src="https://avatars.githubusercontent.com/u/109084569?v=4" alt="Imagem Perfil Github" />
+        </C.Profile>
+        <Flex gap="4px">
+            <Typography>Angelo Hervis</Typography>
+            <Typography weight="300" size="12px" height="14px" >@Angelo_hervis</Typography>
+        </Flex>
     </Flex>
     </C.Container>
 }
