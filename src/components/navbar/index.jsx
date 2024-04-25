5fc2dcc5c48a5c98cfcc2efa6c50d98d2079ce29
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-24 21:07:56 -0300

    atualização NavBar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,39 @@
+import {AiOutlineHome} from "react-icons/ai";
+import {RiGlobalLine} from "react-icons/ri";
+import {FiSend} from "react-icons/fi";
+import {MdMonitor} from "react-icons/md";
+import {BsGear} from "react-icons/bs";
 import {BsSun} from "react-icons/bs";
 import {BiMoon} from "react-icons/bi";
 import {Typography} from "../../style";
 import {Spacer} from "../../style";
 import LogoInstagram from "../../assets/logo-instagram.svg";
 import * as C from "./style";
+import { ListIcon } from "./style";
 import { Flex } from "../../style";
+
+const menuNav = [
+    {
+        icon: <AiOutlineHome/>,
+        menuName: "Início"
+    },
+    {
+        icon: <RiGlobalLine/>,
+        menuName: "Explorar"
+    },
+    {
+        icon: <FiSend/>,
+        menuName: "Direct"
+    },
+    {
+        icon: <MdMonitor/>,
+        menuName: "Reels"
+    },
+    {
+        icon: <BsGear/>,
+        menuName: "Ajustes"
+    },
+]
 
 export function NavBar({themeToggler, theme}) {
     function GroupText({title, subTitle}) {
@@ -40,6 +69,18 @@ export function NavBar({themeToggler, theme}) {
         <GroupText title = "100" subTitle = "Publicações"/>
         <GroupText title = "1627" subTitle = "Seguidores"/>
         <GroupText title = "500" subTitle = "Seguindo"/>
+    </Flex>
+
+    <Spacer/>
+
+    <Flex align="start" gap="16px">
+        {menuNav.map((menu) => (
+            <C.ListIcon key={menu.menuName}>
+                {menu.icon}
+                <Typography>{menu.menuName}</Typography>  
+        </C.ListIcon>
+        ))}   
+
     </Flex>
     </C.Container>
     );
